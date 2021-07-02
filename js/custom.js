4110877dba861a5bad53d2ecd8466544a4c0d417
semyon422 <semyon422@users.noreply.github.com>
2021-07-03 02:19:14 +0500

    Test datatables server side processing

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,21 @@
 function init_data_tables() {
     $(".data-table-all").DataTable({
-        scrollX: true
+        scrollX: true,
     });
 
     $(".data-table-communities").DataTable({
         scrollX: true,
         order: [[ 2, "desc" ]]
+    });
+    $(".data-table-players").DataTable({
+        scrollX: true,
+        serverSide: true,
+        ajax: "/dt/users",
+        columns: [
+            {"data": "name"},
+            {"data": "tag"},
+            {"data": "tag"},
+            {"data": "latest_activity"},
+        ]
     });
 }
