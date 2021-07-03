e157a2f4bb9f0ae1b1eebb19c4495f287fdd135d
semyon422 <semyon422@users.noreply.github.com>
2021-07-03 17:21:11 +0500

    Add datatable initializers for most tables

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,184 @@
 function init_data_tables() {
-    $(".data-table-all").DataTable({
-        scrollX: true,
-    });
-
     $(".data-table-communities").DataTable({
         scrollX: true,
-        order: [[ 2, "desc" ]]
+        serverSide: true,
+        ajax: "/dt/communities",
+        columns: [
+            {
+                data: "name",
+                render(data, type, row, meta) {
+                    return '<a href="/communities/' + row.id + '">' + data + '</a>';
+                }
+            },
+            {data: "alias"},
+            {data: "inputmodes"},
+            {data: "members"},
+            {data: "description"},
+            {data: "name"},
+        ]
+    });
+    $(".data-table-leaderboards").DataTable({
+        scrollX: true,
+        serverSide: true,
+        ajax: "/dt/leaderboards",
+        columns: [
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+        ]
+    });
+    $(".data-table-tables").DataTable({
+        scrollX: true,
+        serverSide: true,
+        ajax: "/dt/tables",
+        columns: [
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+        ]
     });
     $(".data-table-players").DataTable({
         scrollX: true,
         serverSide: true,
         ajax: "/dt/users",
         columns: [
-            {"data": "name"},
-            {"data": "tag"},
-            {"data": "tag"},
-            {"data": "latest_activity"},
+            {data: "name"},
+            {data: "tag"},
+            {data: "tag"},
+            {data: "latest_activity"},
+        ]
+    });
+    $(".data-table-charts").DataTable({
+        scrollX: true,
+        serverSide: true,
+        ajax: "/dt/notecharts",
+        columns: [
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+        ]
+    });
+
+    $(".data-table-community-leaderboards").DataTable({
+        scrollX: true,
+        serverSide: true,
+        ajax: "/dt/communities/" + window.PineconeRouter.currentContext.params.community_id + "/leaderboards",
+        columns: [
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+        ]
+    });
+    $(".data-table-community-players").DataTable({
+        scrollX: true,
+        serverSide: true,
+        ajax: "/dt/communities/" + window.PineconeRouter.currentContext.params.community_id + "/users",
+        columns: [
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+        ]
+    });
+
+    $(".data-table-leaderboard-communities").DataTable({
+        scrollX: true,
+        serverSide: true,
+        ajax: "/dt/leaderboard/" + window.PineconeRouter.currentContext.params.leaderboard_id + "/communities",
+        columns: [
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+        ]
+    });
+    $(".data-table-leaderboard-tables").DataTable({
+        scrollX: true,
+        serverSide: true,
+        ajax: "/dt/leaderboard/" + window.PineconeRouter.currentContext.params.leaderboard_id + "/tables",
+        columns: [
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+        ]
+    });
+    $(".data-table-leaderboard-users").DataTable({
+        scrollX: true,
+        serverSide: true,
+        ajax: "/dt/leaderboard/" + window.PineconeRouter.currentContext.params.leaderboard_id + "/users",
+        columns: [
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+        ]
+    });
+
+    $(".data-table-table-communities").DataTable({
+        scrollX: true,
+        serverSide: true,
+        ajax: "/dt/table/" + window.PineconeRouter.currentContext.params.table_id + "/communities",
+        columns: [
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+        ]
+    });
+    $(".data-table-table-leaderboards").DataTable({
+        scrollX: true,
+        serverSide: true,
+        ajax: "/dt/table/" + window.PineconeRouter.currentContext.params.table_id + "/leaderboards",
+        columns: [
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+        ]
+    });
+    $(".data-table-table-charts").DataTable({
+        scrollX: true,
+        serverSide: true,
+        ajax: "/dt/table/" + window.PineconeRouter.currentContext.params.table_id + "/notecharts",
+        columns: [
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+        ]
+    });
+
+    $(".data-table-chart-scores").DataTable({
+        scrollX: true,
+        serverSide: true,
+        ajax: "/dt/notechart/" + window.PineconeRouter.currentContext.params.notechart_id + "/scores",
+        columns: [
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
+            {data: "name"},
         ]
     });
 }
